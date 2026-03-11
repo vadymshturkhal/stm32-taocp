4293e855e5769dca21392d8a9f0fd9eeb28ebc04
Vadym <vadymshturkhal@gmail.com>
2026-03-12 00:36:17 +0200

    Optimized asm_find_cycle_hack4 which now is ~300 cycles faster

--- multiply_permutations_a/asm_find_cycle_hack4.s
+++ multiply_permutations_a/asm_find_cycle_hack4.s
@@ -7,17 +7,17 @@
 @ Using optimal-sequential write (auto-incrementing)
 @ Use multiplication_result in place
 
-@ Signature
-@ uint32_t find_cycle_hack(char *parsed_permutation,
-@							uint32_t permutation_length,
-@							char* multiplication_result,
-@							uint32_t start_index)
-
-@ Registers:
+@ Input:
 @ R0 parsed_permutation addres and i;
 @ R1 permutation_length;
 @ R2 multiplication_result;
-@ R3 start_index, then parsed_permutation addres + start_index;
+@ R3 start_index;
+
+@ Runtime:
+@ R0 parsed_permutation addres and i;
+@ R1 permutation_length;
+@ R2 multiplication_result;
+@ R3 parsed_permutation addres + start_index;
 @ R4 parsed_permutation addres + permutation_length-1;
 @ R5 start;
 @ R6 current;
@@ -25,8 +25,6 @@
 
 @ Return:
 @ R2 multiplication_result;
-
-@ char *permutation = "(acf)(bd)(abd)(ef)";
 asm_find_cycle_hack4:
 	PUSH {R4-R7, LR}
 
@@ -38,24 +36,22 @@ asm_find_cycle_hack4:
 	MOV R6, R5
 
 	@ R8 = permutation_length-1 + parsed_permutation addres
-	SUB R4, R1, #1
-	ADD R4, R0, R4
+	SUBS R4, R1, #1
+	ADDS R4, R0, R4
 
 	@ Start address
-	ADD R3, R0, R3
+	ADDS R3, R0, R3
 
 @ A4
 init_i:
-	@ R0 = start address
-	MOV R0, R3
+	MOVS R0, R3			@ R0 = start address
 
 .balign 4
 comparing_loop:
 	CMP R0, R4
 	BHS compare_current_and_start
 
-	@ current_char = parsed_permutation[i];
-	LDRB R7, [R0]
+	LDRB R7, [R0], #1	@ current_char = parsed_permutation[i];
 
 	@ Get rid of the tag with AND, UBFX or BIC
 	@ AND R7, R7, 0x7F
@@ -64,25 +60,16 @@ comparing_loop:
 	@ From right to left
 	@ UBFX R7, R7, #0, #7
 
-	@ Clear bit 7 (0x80) only
-	BIC R7, R7, #0x80
+	BIC R7, R7, #0x80	@ Clear bit 7 (0x80) only
 
 	CMP R7, R6
-	BEQ set_new_current
-
-	@ increment R0
-	ADD R0, R0, #1
-	B comparing_loop
+	BNE comparing_loop
 
 set_new_current:
-	@ tag
-	ORR R7, R7, 0x80
-
-	STRB R7, [R0], #1
-
-	@ current = parsed_permutation[i + 1] & 0x7F;
-	LDRB R6, [R0], #1
-	AND R6, R6, 0x7F
+	ORR R7, R7, 0x80	@ tag
+	STRB R7, [R0, #-1]
+	LDRB R6, [R0], #1	@ current = parsed_permutation[i + 1] & 0x7F;
+	BIC R6, R6, #0x80	@ clear tag
 
 	B comparing_loop
 
@@ -97,19 +84,16 @@ place_char_to_result:
 
 @ A6
 found_cycle:
-	MOV R7, #0x29	@ASCII ')'
-
-	@ multiplication_result[multiplication_result_length] = ')';
-	STRB R7, [R2], #1
+	MOV R7, #0x29		@ ASCII ')'
+	STRB R7, [R2], #1	@ multiplication_result[multiplication_result_length] = ')';
 
 	@ Knuth singleton elimination
-	@ multiplication_result_length -= 3 * (multiplication_result[multiplication_result_length - 3] == '(');
-	SUB R12, R2, #3
-	LDRB R7, [R12]
-	CMP	R7, #0x28	@ ASCII '('
+	LDRB R7, [R2, #-3]
+	CMP	R7, #0x28		@ ASCII '('
 	IT EQ
 	SUBEQ R2, R2, #3
 
 done:
 	MOV R0, R2
 	POP {R4-R7, PC}
+
