32f5625fe5ba0e9ec1164c512f0ab46a3b0d3134
Vadym <vadymshturkhal@gmail.com>
2026-03-12 10:44:54 +0200

    added size to Algorithm I

--- inverse_permutation_in_place/comparing_inverse_permutation_in_place.c
+++ inverse_permutation_in_place/comparing_inverse_permutation_in_place.c
@@ -4,6 +4,7 @@
 
 // Prototypes
 void inverse_permutation_in_place(int32_t *permutation, uint32_t permutation_length);
+void asm_inverse_permutation_in_place(int32_t *permutation, uint32_t permutation_length);
 
 
 void comparing_inverse_permutation_in_place() {
@@ -26,7 +27,7 @@ void comparing_inverse_permutation_in_place() {
 
 	// GCC -O3
 	// with uint32_t permutation[] = {0, 6, 2, 1, 5, 4, 3};
-	// cycles_cold = [181-189], cycles_warm = 167;
+	// cycles_cold = [181-189], cycles_warm = 167, size = 100 bytes;
 	start = DWT->CYCCNT;
 	inverse_permutation_in_place(permutation, permutation_length);
 	end = DWT->CYCCNT;
@@ -37,9 +38,10 @@ void comparing_inverse_permutation_in_place() {
 	end = DWT->CYCCNT;
 	volatile uint32_t c_cycles_warm_inverse_permutation_in_place = (end - start) - overhead;
 
+
 	// ARM Assembly
 	// uint32_t permutation[] = {0, 6, 2, 1, 5, 4, 3};
-	// cycles_cold = [151], cycles_warm = 139;
+	// cycles_cold = [150-151], cycles_warm = 139, size = 50 bytes;
 	start = DWT->CYCCNT;
 	asm_inverse_permutation_in_place(permutation, permutation_length);
 	end = DWT->CYCCNT;
@@ -49,6 +51,7 @@ void comparing_inverse_permutation_in_place() {
 	asm_inverse_permutation_in_place(permutation, permutation_length);
 	end = DWT->CYCCNT;
 	volatile uint32_t asm_cycles_warm_inverse_permutation_in_place = (end - start) - overhead;
+
 
 	end = DWT->CYCCNT;
 }
