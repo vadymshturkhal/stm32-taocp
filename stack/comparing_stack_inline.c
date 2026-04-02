68d7bfbb77a0cd1f0739efd3debe25bc8a9d0cd0
Vadym <vadymshturkhal@gmail.com>
2026-04-02 23:00:46 +0300

    uncommented GCC

--- stack/comparing_stack_inline.c
+++ stack/comparing_stack_inline.c
@@ -25,22 +25,22 @@ void comparing_stack_inline() {
 	// Base case: 128 nodes, 128 Push/Pop using Bump Allocator (balloc)
 	// cycles_cold = [4096-4098], cycles_warm = 4045, size = 200 bytes
 	// #pragma unroll 4: cycles_cold = 3662, cycles_warm = 3524, size = 452 bytes
-//	start = DWT->CYCCNT;
-//	volatile uint8_t c_stack_status = perform_c_stack_operations_inline(max_nodes);
-//	if (c_stack_status == 0) return;
-//	end = DWT->CYCCNT;
-//	volatile uint32_t c_stack_cycles_cold = (end - start) - overhead;
-//
-//	start = DWT->CYCCNT;
-//	c_stack_status = perform_c_stack_operations_inline(max_nodes);
-//	if (c_stack_status == 0) return;
-//	end = DWT->CYCCNT;
-//	volatile uint32_t c_stack_cycles_warm = (end - start) - overhead;
+	start = DWT->CYCCNT;
+	volatile uint8_t c_stack_status = perform_c_stack_operations_inline(max_nodes);
+	if (c_stack_status == 0) return;
+	end = DWT->CYCCNT;
+	volatile uint32_t c_stack_cycles_cold = (end - start) - overhead;
+
+	start = DWT->CYCCNT;
+	c_stack_status = perform_c_stack_operations_inline(max_nodes);
+	if (c_stack_status == 0) return;
+	end = DWT->CYCCNT;
+	volatile uint32_t c_stack_cycles_warm = (end - start) - overhead;
 
 
 	// ARM Assembly
 	// Base case: 128 nodes, 128 Push/Pop using Bump Allocator (balloc)
-	// cycles_cold = [2609-2626], cycles_warm = [2553-2555], size = 374 bytes
+	// cycles_cold = [2613-2626], cycles_warm = 2555, size = 374 bytes
 	start = DWT->CYCCNT;
 	asm_stack_status = asm_perform_stack_operations_inline_mve(max_nodes);
 	if (asm_stack_status == 0) return;
