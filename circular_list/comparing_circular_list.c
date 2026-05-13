1747522b49d8629c3803182b07cb4c2bdfcb04c7
Vadym <vadymshturkhal@gmail.com>
2026-05-13 13:55:17 +0300

    updatec clang circular list stats

--- circular_list/comparing_circular_list.c
+++ circular_list/comparing_circular_list.c
@@ -19,9 +19,9 @@ void comparing_circular_lists(void) {
 	if (!run_all_tests()) return 0;  // increases cold cycles run by ~2*max_nodes cycles
 
 	// Useless Cold Measurement Isolation
-	__asm volatile(".balign 16");	// 1. Flash alignment for the benchmark block
-	__DSB();						// 2. Data Synchronization Barrier: Wait for all pending memory stores to finish
-	__ISB();						// 3. Instruction Synchronization Barrier: Flush the CPU pipeline completely
+	// __asm volatile(".balign 16");	// 1. Flash alignment for the benchmark block
+	// __DSB();						// 2. Data Synchronization Barrier: Wait for all pending memory stores to finish
+	// __ISB();						// 3. Instruction Synchronization Barrier: Flush the CPU pipeline completely
 
 	// GCC -O3 -mcpu=cortex-m4 -mthumb: cold cycles = 10318 | warm cycles = 10268 | size = 476 bytes
 	start = DWT->CYCCNT;
@@ -37,7 +37,7 @@ void comparing_circular_lists(void) {
 	volatile uint32_t gcc_circular_list_cycles_warm = (end - start) - overhead;
 
 	// Clang -O3 -mcpu=cortex-m4 -mthumb --target=arm-none-eabi:
-	// cold cycles = 10061 | warm cycles = 10020 | size = 496 bytes
+	// cold cycles = 11879 | warm cycles = 11772 | size = 590 bytes (without clang_asm_circular_list_union)
 	start = DWT->CYCCNT;
 	uint8_t clang_circular_list_status = clang_asm_perform_circular_list_operations(max_nodes);
 	if (clang_circular_list_status == 0) return 0;
