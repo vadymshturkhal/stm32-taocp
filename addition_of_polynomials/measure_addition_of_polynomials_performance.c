3efeb9b5c685ce76e04594eec8c78677e49cf6bb
Vadym <vadymshturkhal@gmail.com>
2026-06-17 10:39:14 +0300

    Created updated ASM Algorithm A version and added create_same_polynomials function to measure_addition_of_polynomials_performance

--- addition_of_polynomials/measure_addition_of_polynomials_performance.c
+++ addition_of_polynomials/measure_addition_of_polynomials_performance.c
@@ -10,13 +10,36 @@
 extern void* asm_balloc(uint32_t size);
 extern void asm_balloc_free(void* memory_pointer);
 extern uint8_t asm_addition_of_polynomials(PolynomialCircularList* polynomial_P, PolynomialCircularList* polynomial_Q);
+extern uint8_t asm_addition_of_polynomials_v1(PolynomialCircularList* polynomial_P, PolynomialCircularList* polynomial_Q);
 
 uint8_t create_polynomials(PolynomialsData* polynomials_data, Polynomials* polynomials);
 uint8_t addition_of_polynomials(PolynomialCircularList* polynomial_P, PolynomialCircularList* polynomial_Q);
 //
 
+uint8_t create_same_polynomials(
+		void** starting_address,
+		PolynomialsData* polynomials_data,
+		Polynomials* polynomials,
+		PolynomialCircularList** polynomial_P,
+		PolynomialCircularList** polynomial_Q
+) {
+	asm_balloc_free(*starting_address);
+
+	int32_t creation_status = create_polynomials(polynomials_data, polynomials);
+	if (creation_status != 0) return 1;
+
+	*polynomial_P = polynomials->polynomial_P;
+	*polynomial_Q = polynomials->polynomial_Q;
+	*starting_address = polynomials->starting_address;
+
+	return 0;
+}
+
 uint8_t measure_addition_of_polynomials_performance(void) {
     volatile uint32_t start, end, overhead;
+    uint8_t creation_status;
+    uint8_t addition_status;
+
 
 	start = DWT->CYCCNT;
 	end = DWT->CYCCNT;
@@ -33,55 +56,73 @@ uint8_t measure_addition_of_polynomials_performance(void) {
 	};
 
 	Polynomials polynomials;
-	uint8_t creation_status = create_polynomials(&polynomials_data, &polynomials);
+	creation_status = create_polynomials(&polynomials_data, &polynomials);
 	if (creation_status != 0) return 1;
 
 	PolynomialCircularList* polynomial_P = polynomials.polynomial_P;
 	PolynomialCircularList* polynomial_Q = polynomials.polynomial_Q;
 	void* starting_address = polynomials.starting_address;
 
-	// Stats for TC4 with TC4_P_SIZE = 128 nodes and TC4_Q_SIZE = 128 nodes
-	// GCC -O3 -mcpu=cortex-m4 -mthumb: cold cycles = 4726 | warm cycles = 4444 | size = 140 bytes (addition_of_polynomials only)
+	// Translation Unit Boundary Stats for TC4 with TC4_P_SIZE = 128 nodes and TC4_Q_SIZE = 128 nodes
+	// GCC -O3 -mcpu=cortex-m4 -mthumb: cold cycles = 4609-4726 | warm cycles = 4440-4444 | size = 140 bytes (addition_of_polynomials only)
 	start = DWT->CYCCNT;
-	addition_of_polynomials(polynomial_P, polynomial_Q);
+	addition_status = addition_of_polynomials(polynomial_P, polynomial_Q);
+	if (addition_status != 0) return addition_status;
 	end = DWT->CYCCNT;
 	volatile uint32_t addition_of_polynomials_cycles_cold = (end - start) - overhead;
 
 	// Create same polynomial_P and polynomial_Q
-	asm_balloc_free(starting_address);
-	create_polynomials(&polynomials_data, &polynomials);
+	creation_status = create_same_polynomials(&starting_address, &polynomials_data, &polynomials, &polynomial_P, &polynomial_Q);
 	if (creation_status != 0) return 1;
-	polynomial_P = polynomials.polynomial_P;
-	polynomial_Q = polynomials.polynomial_Q;
-	starting_address = polynomials.starting_address;
 
 	// Warm run
 	start = DWT->CYCCNT;
-	addition_of_polynomials(polynomial_P, polynomial_Q);
+	addition_status = addition_of_polynomials(polynomial_P, polynomial_Q);
+	if (addition_status != 0) return addition_status;
 	end = DWT->CYCCNT;
 	volatile uint32_t addition_of_polynomials_cycles_warm = (end - start) - overhead;
 
-	// take the result
-	PolynomialNode* Q = polynomial_Q->ptr->link;
-	volatile NodeInfo result_polynomial[polynomial_Q->size-1];  // Without last node
-	Q = polynomial_Q->ptr->link;
-	uint32_t i = 0;
-	while (Q->ABC > 0) {
-		unpack_ABC_32(Q, &result_polynomial[i++]);
+	// Create same polynomial_P and polynomial_Q
+	creation_status = create_same_polynomials(&starting_address, &polynomials_data, &polynomials, &polynomial_P, &polynomial_Q);
+	if (creation_status != 0) return 1;
 
-		// This is equivalent to &result_polynomial[i++]
-//		unpack_ABC_32(Q, result_polynomial + i);
-//		i++;
-
-		Q = Q->link;
-	}
-
+	// Translation Unit Boundary Stats for TC4 with TC4_P_SIZE = 128 nodes and TC4_Q_SIZE = 128 nodes
+	// ASM: cold cycles = 4390 | warm cycles = 4353 | size = 124 bytes (addition_of_polynomials only)
+	// ASM: cold cycles = 4351 | warm cycles = 4313 | size = 124 bytes (addition_of_polynomials only) Placed P = P->link; between polynomial_Q->size--; in A4
+	// ASM: cold cycles = 4305 | warm cycles = 4266 | size = 124 bytes (addition_of_polynomials only) Placed load polynomial size before P = P->link; in A5
+	// ASM: cold cycles = 4259 | warm cycles = 4219 | size = 124 bytes (addition_of_polynomials only) Placed load polynomial size before Q2->link = Q; and advance polynomial size before P = P->link; in A5
+	// ASM: cold cycles = 4219 | warm cycles = 4180 | size = 124 bytes (addition_of_polynomials only) placed .balign 4 before A1
 	start = DWT->CYCCNT;
-	asm_addition_of_polynomials(polynomial_P, polynomial_Q);
+	addition_status = asm_addition_of_polynomials_v1(polynomial_P, polynomial_Q);
+	if (addition_status != 0) return addition_status;
 	end = DWT->CYCCNT;
 	volatile uint32_t asm_addition_of_polynomials_cycles_cold = (end - start) - overhead;
 
+	// Create same polynomial_P and polynomial_Q
+	creation_status = create_same_polynomials(&starting_address, &polynomials_data, &polynomials, &polynomial_P, &polynomial_Q);
+	if (creation_status != 0) return 1;
 
+	start = DWT->CYCCNT;
+	addition_status = asm_addition_of_polynomials_v1(polynomial_P, polynomial_Q);
+	if (addition_status != 0) return addition_status;
+	end = DWT->CYCCNT;
+	volatile uint32_t asm_addition_of_polynomials_cycles_warm = (end - start) - overhead;
+
+//	// take the result
+//	PolynomialNode* Q = polynomial_Q->ptr->link;
+//	volatile NodeInfo result_polynomial[polynomial_Q->size-1];  // Without last node
+//	Q = polynomial_Q->ptr->link;
+//	uint32_t i = 0;
+//	while (Q->ABC > 0) {
+//		unpack_ABC_32(Q, &result_polynomial[i++]);
+//
+//		// This is equivalent to &result_polynomial[i++]
+////		unpack_ABC_32(Q, result_polynomial + i);
+////		i++;
+//
+//		Q = Q->link;
+//	}
+//	//
 
 	asm_balloc_free(starting_address);
 }
