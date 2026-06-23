9402c400beb2e482214c9d029cef4661165da760
Vadym <vadymshturkhal@gmail.com>
2026-06-23 09:32:42 +0300

    Reorganized Registers usage in ASM Algorithm A AVAIL integrated version and updated its stats

--- polynomials/addition_of_polynomials/asm_addition_of_polynomials_integrated_avail.s
+++ polynomials/addition_of_polynomials/asm_addition_of_polynomials_integrated_avail.s
@@ -24,83 +24,75 @@
 @ R1 PolynomialCircularList* polynomial_Q
 
 @ Runtime:
-@ R0 PolynomialCircularList* polynomial_P, P->ABC, scratch
-@ R1 PolynomialCircularList* polynomial_Q, Q->ABC, scratch
+@ R0 PolynomialCircularList* polynomial_P, PolynomialNode* P
+@ R1 PolynomialCircularList* polynomial_Q
 @ R2 scratch
 @ R3 scratch
-@ R4 P
+@ R4 AVAIL
 @ R5 Q
 @ R6 Q1
-@ R7 AVAIL
-@ R8 polynomial_Q
 
 @ Return: status
 
 asm_addition_of_polynomials_integrated_avail:
-	PUSH {R4-R8, LR}
+	PUSH {R4-R6, LR}
 
-	@ Storage_Pool* storage_pool = polynomial_Q->storage_pool;
-	LDR R2, [R1, #POLYNOMIAL_STORAGE_POOL]
-
-	@ PolynomialNode* P = polynomial_P->ptr;
-	LDR R4, [R0, #POLYNOMIAL_PTR]
-
-	@ PolynomialNode* Q = polynomial_Q->ptr;
-	LDR R5, [R1, #POLYNOMIAL_PTR]
-
-	LDR R7, [R2, #STORAGE_POOL_AVAIL]
-	MOVS R8, R1					@ save polynomial_Q
+	LDR R2, [R1, #POLYNOMIAL_STORAGE_POOL]	@ Load polynomial_Q->storage_pool
+	LDR R0, [R0, #POLYNOMIAL_PTR]			@ P = polynomial_P->ptr
+	LDR R5, [R1, #POLYNOMIAL_PTR]			@ Q = polynomial_Q->ptr
+	LDR R4, [R2, #STORAGE_POOL_AVAIL]		@ Load AVAIL
 
 .balign 4
 @ A1. [Initialize]
 A1:
-	LDR R4, [R4, #NODE_LINK]	@ P = P->link;
-	MOVS R6, R5 				@ Q1 = Q;
-	LDR R5, [R5, #NODE_LINK]	@ Q = Q->link;
+	LDR R0, [R0, #NODE_LINK]	@ P = P->link
+	MOVS R6, R5 				@ Q1 = Q
+	LDR R5, [R5, #NODE_LINK]	@ Q = Q->link
 
 init_A2:
-	LDR R0, [R4, #NODE_ABC]
+	LDR R2, [R0, #NODE_ABC]		@ P->ABC
 
 @ A2. [ABC(P):ABC(Q)]
 .balign 4
 A2:
-	LDR R1, [R5, #NODE_ABC]
+	LDR R3, [R5, #NODE_ABC]		@ Q->ABC
 
-	CMP R0, R1
+	CMP R2, R3
 	BEQ A3
 	BGT A5
 
 	@ less than
-	MOVS R6, R5 				@ Q1 = Q;
-	LDR R5, [R5, #NODE_LINK]	@ Q = Q->link;
+	MOVS R6, R5 				@ Q1 = Q
+	LDR R5, [R5, #NODE_LINK]	@ Q = Q->link
 	B A2
 
 @ A3. [Add coefficients]
 A3:
-	CMP R0, #0
+	CMP R2, #0
 	BLT done
 
-	LDR R0, [R4, #NODE_COEFF]	@ P->COEFF
-	LDR R1, [R5, #NODE_COEFF]	@ Q->COEFF
+	LDR R2, [R0, #NODE_COEFF]	@ P->COEFF
+	LDR R3, [R5, #NODE_COEFF]	@ Q->COEFF
 
-	ADDS R0, R0, R1				@ P->COEFF + Q->COEFF
-	STR R0, [R5, #NODE_COEFF]	@ Q->COEFF = P->COEFF + Q->COEFF
+	ADDS R3, R3, R2				@ P->COEFF + Q->COEFF
+	STR R3, [R5, #NODE_COEFF]	@ Q->COEFF = P->COEFF + Q->COEFF
 	BNE A1						@ if not zero go to A1
 
 @ A4. [Delete zero term]
 A4:
-	MOVS R1, R5					@ Q2 = Q
-	LDR R5, [R5, #NODE_LINK]	@ Q = Q->link
-	STR R5, [R6, #NODE_LINK]	@ Q1->link = Q
+	MOVS R2, R5						@ Q2 = Q
+	LDR R5, [R5, #NODE_LINK]		@ Q = Q->link
+	STR R5, [R6, #NODE_LINK]		@ Q1->link = Q
 
-	LDR R0, [R8, #POLYNOMIAL_SIZE]	@ load polynomial size
+	LDR R3, [R1, #POLYNOMIAL_SIZE]	@ load polynomial size
+
 	@ AVAIL <= Q2
-	STR R7, [R1, #NODE_LINK]	@ Q2->link = AVAIL
-	SUBS R0, R0, #1				@ polynomial size--;
-	MOVS R7, R1					@ AVAIL = Q2
+	STR R4, [R2, #NODE_LINK]		@ Q2->link = AVAIL
+	SUBS R3, R3, #1					@ polynomial size--
+	MOVS R4, R2						@ AVAIL = Q2
 
-	LDR R4, [R4, #NODE_LINK]	@ P = P->link;
-	STR R0, [R8, #POLYNOMIAL_SIZE]	@ store polynomial size;
+	LDR R0, [R0, #NODE_LINK]		@ P = P->link;
+	STR R3, [R1, #POLYNOMIAL_SIZE]	@ store polynomial size
 
 	@ B A2
 	B init_A2
@@ -108,26 +100,25 @@ A4:
 @ A5. [Insert new term]
 A5:
 	@ Q2 <= AVAIL
-	MOVS R0, R7
-	CBZ R0, overflow
-	LDR R7, [R7, #NODE_LINK]	@ R0 = Q
+	MOVS R3, R4					@ R3 = Q
+	CBZ R3, overflow
+	LDR R4, [R4, #NODE_LINK]	@ AVAIL = AVAIL->link
 
-	LDR R1, [R4, #NODE_COEFF]	@ P->COEFF
-	LDR R2, [R4, #NODE_ABC]		@ P->ABC
-	STR R1, [R0, #NODE_COEFF]	@ Q->COEFF = P->COEFF
-	STR R2, [R0, #NODE_ABC]		@ Q->ABC = P->ABC
+	STR R2, [R3, #NODE_ABC]		@ Q->ABC = P->ABC (R2 is P->ABC after A2)
+	LDR R2, [R0, #NODE_COEFF]	@ P->COEFF
+	STR R2, [R3, #NODE_COEFF]	@ Q->COEFF = P->COEFF
 
 	// LDMIA + STMIA are slower than 2 LDR + 2 STR
 
-	LDR R1, [R8, #POLYNOMIAL_SIZE]	@ load polynomial size
-	STR R5, [R0, #NODE_LINK]	@ Q2->link = Q;
-	STR R0, [R6, #NODE_LINK]	@ Q1->link = Q2;
-	ADDS R1, R1, #1				@ advance polynomial size
+	LDR R2, [R1, #POLYNOMIAL_SIZE]	@ load polynomial size
+	STR R5, [R3, #NODE_LINK]		@ Q2->link = Q;
+	STR R3, [R6, #NODE_LINK]		@ Q1->link = Q2;
+	ADDS R2, R2, #1					@ advance polynomial size
 
-	LDR R4, [R4, #NODE_LINK]	@ P = P->link;
+	LDR R0, [R0, #NODE_LINK]		@ P = P->link;
 
 	@ polynomial_Q->size++;
-	STR R1, [R8, #POLYNOMIAL_SIZE]	@ store polynomial size
+	STR R2, [R1, #POLYNOMIAL_SIZE]	@ store polynomial size
 
 	B init_A2 						@ Go to step 2
 
@@ -139,6 +130,6 @@ overflow:
 	MOVS R0, #1
 
 exit:
-	LDR R1, [R4, #POLYNOMIAL_STORAGE_POOL]
-	STR R7,	[R1, #STORAGE_POOL_AVAIL]		@ sync AVAIL
-	POP {R4-R8, PC}
+	LDR R2, [R1, #POLYNOMIAL_STORAGE_POOL]	@ Load storage_pool
+	STR R4,	[R2, #STORAGE_POOL_AVAIL]		@ Store AVAIL
+	POP {R4-R6, PC}
