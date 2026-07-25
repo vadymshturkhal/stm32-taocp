fac5b1fc2ee7d4e37c93fb0de771a7c810f081a3
Vadym <vadymshturkhal@gmail.com>
2026-07-25 14:51:14 +0300

    updated decision to use methods keys

--- elevator/decision.py
+++ elevator/decision.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import asyncio
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from elevator import Elevator
@@ -11,11 +12,13 @@ def decision(elevator: Elevator, caller: str | None = None):
     # D1 [Decision necessary?]
     if elevator.STATE != STATE.NEUTRAL:
         return
-    
+
     # D2 [Should door open?]
-    if elevator.is_running("E1") and elevator.SHARED_STATE.CALLS[INITIAL_FLOOR] != 0:
+    if elevator.is_running(elevator.E1) and elevator.SHARED_STATE.CALLS[INITIAL_FLOOR] != 0:
         # if elevator positioned at E1: after 20 units of time start E3 and exit
-        elevator.start_after(UNIT * 20, "E3")
+        elevator.cancel(elevator.E3)
+        task = asyncio.create_task(elevator.E3(UNIT * 20))
+        elevator.tasks[elevator.E3] = task
         return
 
     # D3 [Any calls?]
@@ -41,7 +44,9 @@ def decision(elevator: Elevator, caller: str | None = None):
         elevator.STATE = STATE.GOINGUP
 
     # D5 [Elevator dormant]
-    if elevator.is_running("E1") and j != INITIAL_FLOOR:
+    if elevator.is_running(elevator.E1) and j != INITIAL_FLOOR:
         # Prepare to move
-        elevator.start_after(UNIT * 20, "E6")
+        elevator.cancel(elevator.E6)
+        task = asyncio.create_task(elevator.E6(UNIT * 20))
+        elevator.tasks[elevator.E6] = task
     return
