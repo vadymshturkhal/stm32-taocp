bdfc3b731699224cbc1235796f8a9085f09ddcb1
Vadym <vadymshturkhal@gmail.com>
2026-08-08 20:22:32 +0300

    Added Elevator

---
+++ elevator/C_WAIT_LIST/elevator.c
@@ -0,0 +1,31 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "elevator.h"
+#include "main.h"
+
+uint32_t elevator_init(Elevator* elevator, SharedState* shared_state, Storage_Pool* storage_pool) {
+	if (elevator == NULL || shared_state == NULL || storage_pool == NULL) return 1;
+
+	elevator->shared_state = shared_state;
+	shared_state->elevator = elevator;   // add Elevator to shared state
+
+	elevator->ELEV1 = storage_pool_pop(storage_pool);
+	if (elevator->ELEV1 == NULL) return 2;
+
+	elevator->ELEV2 = storage_pool_pop(storage_pool);
+	if (elevator->ELEV2 == NULL) return 2;
+
+	elevator->ELEV3 = storage_pool_pop(storage_pool);
+	if (elevator->ELEV3 == NULL) return 2;
+
+	elevator->STATE = 0;  // Neutral
+
+	elevator->D1 = 0;
+	elevator->D2 = 0;
+	elevator->D3 = 0;
+
+	elevator->first_search = false;
+
+	return 0;
+}
