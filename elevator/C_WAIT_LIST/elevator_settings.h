e3d3f966243b7768a81aff428314f23a4827bcb6
Vadym <vadymshturkhal@gmail.com>
2026-08-08 20:01:24 +0300

    Added shared state to C

--- elevator/C_WAIT_LIST/elevator_settings.h
+++ elevator/C_WAIT_LIST/elevator_settings.h
@@ -3,22 +3,44 @@
 
 #include <stdint.h>
 
+#include "storage_pool.h"
+
+#define FLOORS 5
+
+#define CALLUP   0b100
+#define CALLDOWN 0b010
+#define CALLCAR  0b001
+
 typedef void (*NextInst)(struct ElevatorNode*);
 
-// Without INTERTIME and USER_NAME
+// NOTE: Without INTERTIME and USER_NAME
 typedef struct ElevatorNode {
     struct ElevatorNode* left1;  // WAIT list
     struct ElevatorNode* right1;
     struct ElevatorNode* left2;  // QUEUE, ELEVATOR_LIST
     struct ElevatorNode* right2;
-
     uint32_t nexttime;
     NextInst nextinst;
+    uint32_t in;                 // User
+    uint32_t out;                // User
+    uint32_t giveuptime;         // User
+} ElevatorNode;
 
-    uint32_t in;                 // UserInfo.IN
-    uint32_t out;                // UserInfo.OUT
-    uint32_t giveuptime;         // UserInfo.GIVEUPTIME
-} DoublyNode;
+typedef struct {
+	ElevatorNode* head;
+	Storage_Pool* storage_pool;
+	uint32_t size;
+} ElevatorList;
 
+// NOTE: Add Elevator and Users
+typedef struct {
+    uint32_t time;
+    uint32_t calls[FLOORS];
+    ElevatorList elevator_list;
+    ElevatorList queue[FLOORS];
+    ElevatorList wait_list;
+} SharedState;
+
+uint32_t elevator_list_init(ElevatorList* elevator_list, Storage_Pool* storage_pool);
 
 #endif
