04c3dfbc3a81d62993d4c88619582c74d1f8350c
Vadym <vadymshturkhal@gmail.com>
2026-09-08 22:12:32 +0300

    added clever way of mod calculation

--- linear_congruential_method/linear_congruential_sequence.py
+++ linear_congruential_method/linear_congruential_sequence.py
@@ -11,8 +11,7 @@ class LinearCongruentialMethod:
     the result given modulo w
     """
 
-    def __init__(self, m=10, a=7, c=0, X_0=7):
-        # self.m = m
+    def __init__(self, a=7, c=0, X_0=7):
         self.a = a
         self.c = c
         self.X = X_0
@@ -20,22 +19,57 @@ class LinearCongruentialMethod:
         self.w = 2**self.e
         self.m = self.w + 1
     
+    # def next_number(self):
+    #     """
+    #     Computes (aX + c) mod w.
+
+    #     MIX instructions:
+    #     LDA A
+    #     MUL X
+    #     SLAX 5
+    #     ADD C
+    #     """
+
+    #     X = self.X
+    #     mask = self.w - 1
+    #     product = self.a * self.X
+    #     lower_half_result = product & mask
+    #     self.X = (lower_half_result + self.c) & mask
+    #     return X
+
     def next_number(self):
         """
-        Computes (aX + c) mod w.
-
-        MIX instructions:
-        LDA A
-        MUL X
-        SLAX 5
-        ADD C
+        Computes (aX) mod m, where m = w + 1
         """
 
         X = self.X
-        mask = self.w - 1
-        product = self.a * self.X
-        lower_half_result = product & mask
-        self.X = (lower_half_result + self.c) & mask
+        self.X = (self.a * self.X) % (self.w + 1)
+        return X
+
+    def next_number_clever(self):
+        """
+        Computes (aX) mod m, where m = w + 1
+
+        MIX instructions:
+        LDAN X      # load negative X to rA
+        MUL A       # multiply rA by A, upper bytes are placed in rA, lower in rX
+        STX TEMP
+        SUB TEMP
+        JANN *+3
+        INCA 2
+        ADD =w-l=
+        """
+
+        X = self.X
+        product = X * self.a
+        q, r = divmod(product, self.w)  # MUL: high word q (rA), low word r (rX)
+        A = r - q                        # STX TEMP; SUB TEMP
+
+        if A < 0:
+            A += 2          # INCA 2
+            A += self.w - 1  # ADD =w-l=
+
+        self.X = A
         return X
 
 def check_sequence(sequence):
@@ -70,6 +104,24 @@ if __name__ == "__main__":
     print(f"{first_term = }")
     for _ in range(1000):
         next_number = LCM.next_number()
+        sequence.append(next_number)
+
+        if len(sequence) < 4:
+            continue
+
+        is_found_periond = check_sequence(sequence)
+
+        if is_found_periond:
+            # print(sequence)
+            period = retrieve_period(sequence)
+            print(f"{period = }")
+            print(f"{len(period) = }")
+            break
+
+    print()
+    sequence.clear()
+    for _ in range(1000):
+        next_number = LCM.next_number_clever()
         sequence.append(next_number)
 
         if len(sequence) < 4:
